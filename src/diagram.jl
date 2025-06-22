884960e86f471050fba22607d237e916b9431ae6
johan/n <j.b.kleindopf@gmail.com>
2025-06-22 14:47:43 +0200

    Set up struct to keep track of voronoi diagram

---
+++ src/diagram.jl
@@ -0,0 +1,31 @@
+# DCEL data structures for Voronoi diagram representation
+
+module DCEL
+
+export Vertex, HalfEdge, Face, DCEL
+
+struct Vertex   # Represents a point in the plane, placed by a player.
+    x::Float64
+    y::Float64
+end
+
+struct HalfEdge
+    origin::Int           # Index of vertice from which half-edge emanates.
+    twin::Int             # Index from halfedges array (the dual edge in the opposite direction).
+    next::Int             # Next halfedge of traversal sequence (given by it's index).
+    prev::Int             # Previous halfedge in the traversal sequence (given by it's index).
+    incident_face::Int    # Index of face that borders half-edge to the left (when traversing).
+end
+
+struct Face
+    site::Int             # Index of generator point placed by a player.
+    outer_component::Int  # Index into halfedges array (one of the bounding edges).
+end
+
+struct DCELStruct
+    vertices::Vector{Vertex}
+    halfedges::Vector{HalfEdge}
+    faces::Vector{Face}
+end
+
+end
