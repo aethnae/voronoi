19d069e90fcdaa59eb1b93334aa5ddc5f9475867
johan/n <j.b.kleindopf@gmail.com>
2025-07-03 15:59:55 +0200

    added controller.jl file for game states and winner functions. Still WIP

--- src/Voronoi.jl
+++ src/Voronoi.jl
@@ -5,6 +5,8 @@ using GtkObservables.Gtk4
 using GtkObservables.Cairo
 
 export run_tests
+export GameState, new_game, place_point!, game_over, compute_areas, winner
+export Delaunay, Vertex, insert_point!, voronoi, areas
 
 run_tests() = include("../test/runtests.jl")
 
@@ -12,5 +14,6 @@ include("dcel.jl")
 include("triangulation.jl")
 include("polygons.jl")
 include("gui.jl")
+include("controller.jl")
 
 end # module Voronoi
